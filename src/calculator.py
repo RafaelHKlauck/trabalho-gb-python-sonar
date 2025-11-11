c23a34f2c16b35b989bfea6cd13307cfe2380b67
Rafael Klauck <rhansenklauck@gmail.com>
2025-11-11 13:06:18 -0300

    teste

---
+++ src/calculator.py
@@ -0,0 +1,28 @@
+class Calculator:
+  def __init__(self, a, b):
+    self.a = a
+    self.b = b
+
+  def add(self):
+    return self.a + self.b
+
+  def subtract(self):
+    return self.a - self.b
+
+  def multiply(self):
+    return self.a * self.b
+    
+  def divide(self):
+    if self.b == 0:
+      return "Error: Division by zero"
+    return self.a / self.b
+
+  def power(self):
+    return self.a ** self.b
+
+  def square(self):
+    return self.a ** 2
+
+  def cube(self):
+    return self.a ** 3
+  
