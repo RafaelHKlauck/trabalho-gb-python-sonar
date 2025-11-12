e522925862043647f38c7a074e0e5aa9169cda08
Rafael Klauck <rhansenklauck@gmail.com>
2025-11-11 22:29:12 -0300

    criando demo

--- src/calculator.py
+++ src/calculator.py
@@ -1,27 +1,5 @@
-class Calculator:
-  def __init__(self, a, b):
-    self.a = a
-    self.b = b
+target = -5
+num = 3
 
-  def add(self):
-    return self.a + self.b
-
-  def subtract(self):
-    return self.a - self.b
-
-  def multiply(self):
-    return self.a * self.b
-    
-  def divide(self):
-    if self.b == 0: 
-      return "Error: Division by zero"
-    return self.a / self.b
-
-  def power(self):
-    return self.a ** self.b
-
-  def square(self):
-    return self.a ** 2
-
-  def cube(self):
-    return self.a ** 3
+target =- num  # Noncompliant: target = -3. Is that really what's meant?
+target =+ num # Noncompliant: target = 3
